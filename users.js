192d5ce8f99e1c7e344f796ff5d265633a6b6f05
elumbantoruan418-collab <elumbantoruan418@gmail.com>
2026-01-23 20:24:36 +0700

    Update users.js

--- users.js
+++ users.js
@@ -1,5 +1,39 @@
 // users.js
-const USERS = [
-  { username: "winz", password: "001" },
-  { username: "winz", password: "winz123" }
-];
+let users = JSON.parse(localStorage.getItem("users")) || [];
+
+function saveUsers() {
+  localStorage.setItem("users", JSON.stringify(users));
+}
+
+function registerUser(username, password) {
+  const id = "U" + Date.now();
+
+  const user = {
+    id: id,
+    username: username,
+    password: password,
+    saldo: 0
+  };
+
+  users.push(user);
+  saveUsers();
+
+  // set user aktif
+  localStorage.setItem("currentUser", id);
+
+  return user;
+}
+
+function getCurrentUser() {
+  const id = localStorage.getItem("currentUser");
+  return users.find(u => u.id === id);
+}
+
+function updateUserSaldo(userId, newSaldo) {
+  const user = users.find(u => u.id === userId);
+  if (!user) return false;
+
+  user.saldo = newSaldo;
+  saveUsers();
+  return true;
+}
