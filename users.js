cf98759946daabe673342dea778f078534dfd271
elumbantoruan418-collab <elumbantoruan418@gmail.com>
2026-01-28 14:20:10 +0700

    Update users.js

--- users.js
+++ users.js
@@ -1,6 +1,21 @@
-const users= [
+// ambil user dari localStorage
+let USERS = JSON.parse(localStorage.getItem("USERS")) || [];
+
+// admin (hardcode)
+const ADMINS = [
   {
+    id: "ADMIN-1",
     username: "winz",
     password: "001"
   }
 ];
+
+// simpan user
+function saveUsers() {
+  localStorage.setItem("USERS", JSON.stringify(USERS));
+}
+
+// generate ID otomatis
+function generateUserId() {
+  return "USR-" + Date.now();
+}
