fef1a831dcb9e9658fcd522f467b5a079b27c8e6
elumbantoruan418-collab <elumbantoruan418@gmail.com>
2026-01-29 18:18:24 +0700

    Update users.js

--- users.js
+++ users.js
@@ -6,36 +6,14 @@ const USERS = [
     id: 8427342247,
     username: "winz",
     password: "001",
+    apiKey: "W_8f2kA9Qxw1",
     createdAt: "2025-01-20"
   },
   {
     id: 7391842561,
     username: "user1",
     password: "12345",
+    apiKey: "U_Lp29ZxM81Q",
     createdAt: "2025-01-21"
   }
 ];
-
-// =======================
-// SAVE USERS TO STORAGE
-// =======================
-function saveUsers() {
-  localStorage.setItem("USERS", JSON.stringify(USERS));
-}
-
-// =======================
-// LOAD USERS FROM STORAGE
-// =======================
-function loadUsers() {
-  const storedUsers = localStorage.getItem("USERS");
-
-  if (!storedUsers) return;
-
-  USERS.length = 0;
-  USERS.push(...JSON.parse(storedUsers));
-}
-
-// =======================
-// AUTO LOAD ON PAGE OPEN
-// =======================
-loadUsers();
